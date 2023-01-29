e3870c3f3b60627e63c414d064572e12c70414d5
zxcvb2002 <jbyjby3743@naver.com>
2023-01-29 17:12:13 +0900

    The offset of the GeometryReader's View was adjusted to the center.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -57,7 +57,7 @@ struct ContentView: View {
                 }))
                 .offset(self.size)
             }
-        }
+        }.offset(x: 25, y: 250)
         .animation(.spring())
     }
 }
